f5efecce0a797889a1da229e000672c317076131
Caleb Spare <cespare@gmail.com>
2012-06-01 12:58:21 -0700

    Start working on piece drawing logic.

--- tetris.go
+++ tetris.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	width  = 20
+	width  = 10
 	height = 10
 )
 
@@ -20,18 +20,30 @@ const (
 )
 
 type Board struct {
-	Cells [height][width]bool
+	cells [height][width]bool
+	currentPiece *Piece
+	currentX, currentY int
+}
+
+type Game struct {
+	board *Board
+	nextPiece *Piece
 }
 
 type Piece interface {
 	Initialize(*Board) bool
 	Move(*Board, direction) bool
 	Rotate(*Board, direction) bool
+	Filled(x, y int) bool
+}
+
+func (board *Board) Filled(x, y int) bool {
+	return board.cells[y][x] && board.currentPiece.Filled(x - board.currentX, y - board.currentY)
 }
 
 func (board *Board) Draw() {
 	fmt.Println("+" + strings.Repeat("-", width) + "+")
-	for _, row := range board.Cells {
+	for _, row := range board.cells {
 		fmt.Printf("|")
 		for _, cell := range row {
 			if cell {
