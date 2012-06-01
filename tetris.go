05154f58d2218e013b0cf5ab05b88b0ad64aa684
Caleb Spare <cespare@gmail.com>
2012-06-01 02:20:16 -0700

    Some preliminaries.

--- tetris.go
+++ tetris.go
@@ -1,3 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
 
+const (
+	width  = 20
+	height = 10
+)
+
+type direction int
+
+const (
+	up direction = iota + 1
+	down
+	left
+	right
+)
+
+type Board struct {
+	Cells [height][width]bool
+}
+
+type Piece interface {
+	Initialize(*Board) bool
+	Move(*Board, direction) bool
+	Rotate(*Board, direction) bool
+}
+
+func (board *Board) Draw() {
+	fmt.Println("+" + strings.Repeat("-", width) + "+")
+	for _, row := range board.Cells {
+		fmt.Printf("|")
+		for _, cell := range row {
+			if cell {
+				fmt.Printf("#")
+			} else {
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Println("|")
+	}
+	fmt.Println("+" + strings.Repeat("-", width) + "+")
+}
+
+func main() {
+	board := new(Board)
+	board.Draw()
+}
